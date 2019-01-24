c7dc13716b2099d1d3d9e6d10d51006c76c60126
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2019-01-24 20:56:25 +0200

    Login class to FB executed

--- Finding_Price/Form1.cs
+++ Finding_Price/Form1.cs
@@ -16,5 +16,42 @@ namespace Finding_Price
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("www.facebook.com/");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                HtmlElement user = webBrowser1.Document.GetElementById("email");
+                HtmlElement password = webBrowser1.Document.GetElementById("pass");
+
+
+                user.SetAttribute("value", textBox1.Text);
+                password.SetAttribute("value", textBox2.Text);
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Error!");
+            }
+            
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HtmlElement btnClick = webBrowser1.Document.GetElementById("u_0_8");
+
+            btnClick.InvokeMember("click");
+
+        }
+
+
     }
 }
